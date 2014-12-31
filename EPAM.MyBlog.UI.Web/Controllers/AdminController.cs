03534f5156893f47eb771f12764118bea1421e55
Evgeniy J <jemguns@gmail.com>
2014-12-31 10:05:32 +0400

    I'm broken, admins userlist, loock posts, deleting post problems

--- EPAM.MyBlog.UI.Web/Controllers/AdminController.cs
+++ EPAM.MyBlog.UI.Web/Controllers/AdminController.cs
@@ -33,5 +33,42 @@ namespace EPAM.MyBlog.UI.Web.Controllers
             return View(UserAdminModel.GetAllUsers());
         }
 
+        [Authorize(Roles = "Admin")]
+        public ActionResult Posts(string name)
+        {
+            return View(PresentPostModel.GetAllPostsTitle(name));
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Comments()
+        {
+            return PartialView();
+
+        }
+
+
+        public ActionResult Delete(Guid Id)
+        {
+            var post = PostModel.GetPostById(Id);
+            ViewData["Title"] = post.Title;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Guid id, ConfirmModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (PostModel.Delete(id))
+                {
+                    return RedirectToAction("Index", "Post");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
     }
 }
